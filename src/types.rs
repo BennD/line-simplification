e5951cee15737fbd6af2891fe182d4ca897dc54a
Benno Doerr <bencayd@gmail.com>
2021-11-19 22:36:33 +0100

    add more Point conversion
    Point <-> [f32; 2]
    Point <-> (f32, f32)
    Point <-> emath::Pos2

--- src/types.rs
+++ src/types.rs
@@ -10,8 +10,87 @@ pub struct Line {
     pub end: Point,
 }
 
+// ----------------------------------------------------------------------------
+// Compatibility and convenience conversions to and from [f32; 2]:
+
 impl From<[f32; 2]> for Point {
-    fn from(o: [f32; 2]) -> Self {
-        Point { x: o[0], y: o[1] }
+    fn from(v: [f32; 2]) -> Self {
+        Point { x: v[0], y: v[1] }
+    }
+}
+
+impl From<&[f32; 2]> for Point {
+    fn from(v: &[f32; 2]) -> Self {
+        Point { x: v[0], y: v[1] }
+    }
+}
+
+impl From<Point> for [f32; 2] {
+    fn from(v: Point) -> [f32; 2] {
+        [v.x, v.y]
+    }
+}
+
+impl From<&Point> for [f32; 2] {
+    fn from(v: &Point) -> [f32; 2] {
+        [v.x, v.y]
+    }
+}
+
+// ----------------------------------------------------------------------------
+// Compatibility and convenience conversions to and from (f32, f32):
+
+impl From<(f32, f32)> for Point {
+    fn from(v: (f32, f32)) -> Self {
+        Point { x: v.0, y: v.1 }
+    }
+}
+
+impl From<&(f32, f32)> for Point {
+    fn from(v: &(f32, f32)) -> Self {
+        Point { x: v.0, y: v.1 }
+    }
+}
+
+impl From<Point> for (f32, f32) {
+    fn from(v: Point) -> (f32, f32) {
+        (v.x, v.y)
+    }
+}
+
+impl From<&Point> for (f32, f32) {
+    fn from(v: &Point) -> (f32, f32) {
+        (v.x, v.y)
+    }
+}
+
+// ----------------------------------------------------------------------------
+// egui compatibility and convenience conversions
+
+#[cfg(feature = "emath")]
+impl From<emath::Pos2> for Point {
+    fn from(v: emath::Pos2) -> Self {
+        Point { x: v.x, y: v.y }
+    }
+}
+
+#[cfg(feature = "emath")]
+impl From<&emath::Pos2> for Point {
+    fn from(v: &emath::Pos2) -> Self {
+        Point { x: v.x, y: v.y }
+    }
+}
+
+#[cfg(feature = "emath")]
+impl From<Point> for emath::Pos2 {
+    fn from(v: Point) -> Self {
+        emath::Pos2 { x: v.x, y: v.y }
+    }
+}
+
+#[cfg(feature = "emath")]
+impl From<&Point> for emath::Pos2 {
+    fn from(v: &Point) -> Self {
+        emath::Pos2 { x: v.x, y: v.y }
     }
 }
